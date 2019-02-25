8fe7faf02cb1627e4077186b3304afbd0642fdfd
Glenn Ergeerts <glenn.ergeerts@aloxy.io>
2019-02-25 09:52:45 +0100

    update to changed API of modem driver + use D7 only for now

--- apps/sensor_push/main.c
+++ apps/sensor_push/main.c
@@ -21,7 +21,7 @@ An unsolicited message will be transmitted periodically using the DASH7 interfac
 #define LORAWAN_DEV_ADDR 0x00000000
 #define LORAWAN_NETW_ID 0x000000
 
-void on_modem_command_completed_callback(bool with_error) 
+void on_modem_command_completed_callback(bool with_error)
 {
     printf("modem command completed (success = %i)\n", !with_error);
 }
@@ -36,7 +36,9 @@ void on_modem_write_file_data_callback(uint8_t file_id, uint32_t offset, uint32_
     printf("modem write file data file %i offset %li size %li buffer %p\n", file_id, offset, size, output_buffer);
 }
 
-static d7ap_session_config_t d7_session_config = {
+static session_config_t session_config = {
+  .interface_type = DASH7,
+  .d7ap_session_config = {
     .qos = {
         .qos_resp_mode = SESSION_RESP_MODE_PREFERRED,
         .qos_retry_mode = SESSION_RETRY_MODE_NO
@@ -50,53 +52,32 @@ static d7ap_session_config_t d7_session_config = {
         .access_class = 0x01,
         .id = {0},
     },
-};
-  
-static lorawan_session_config_abp_t lorawan_session_config = {
-    .appSKey = LORAWAN_APP_SESSION_KEY,
-    .nwkSKey = LORAWAN_NETW_SESSION_KEY,
-    .devAddr = LORAWAN_DEV_ADDR,
-    .request_ack = false,
-    .network_id = LORAWAN_NETW_ID,
-    .application_port = 1
+  }
 };
 
 int main(void)
 {
     puts("Welcome to RIOT!");
-    
+
     modem_callbacks_t modem_callbacks = {
         .command_completed_callback = &on_modem_command_completed_callback,
         .return_file_data_callback = &on_modem_return_file_data_callback,
         .write_file_data_callback = &on_modem_write_file_data_callback,
     };
 
-    modem_init(UART_DEV(1), &modem_callbacks);
+    modem_init(1, 9600);
+    modem_cb_init(&modem_callbacks);
 
     uint8_t uid[D7A_FILE_UID_SIZE];
     modem_read_file(D7A_FILE_UID_FILE_ID, 0, D7A_FILE_UID_SIZE, uid);
     printf("modem UID: %02X%02X%02X%02X%02X%02X%02X%02X\n", uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], uid[6], uid[7]);
-    
+
     xtimer_ticks32_t last_wakeup = xtimer_now();
-    alp_itf_id_t current_interface_id = ALP_ITF_ID_D7ASP;
-    void* current_interface_config = (void*)&d7_session_config;
     uint8_t counter = 0;
     while(1) {
         printf("Sending msg with counter %i\n", counter);
         uint32_t start = xtimer_now_usec();
-        if(counter % 5 == 0) {
-            if(current_interface_id == ALP_ITF_ID_D7ASP) {
-                printf("Switching to LoRaWAN\n");
-                current_interface_id = ALP_ITF_ID_LORAWAN_ABP;
-                current_interface_config = &lorawan_session_config;
-            } else {
-                printf("Switching to D7AP\n");
-                current_interface_id = ALP_ITF_ID_D7ASP;
-                current_interface_config = &d7_session_config;
-            }
-        }
-
-        modem_status_t status = modem_send_unsolicited_response(0x40, 0, 1, &counter, current_interface_id, current_interface_config);
+        modem_status_t status = modem_send_unsolicited_response(0x40, 0, 1, &counter, &session_config);
         uint32_t duration_usec = xtimer_now_usec() - start;
         printf("Command completed in %li ms\n", duration_usec / 1000);
         if(status == MODEM_STATUS_COMMAND_COMPLETED_SUCCESS) {
